19e278d2d96a9a7a91af46ce8771853f29e0b03d
zhangheng <zhangheng@moguyun.com>
2019-07-29 16:37:03 +0800

    chore：给常用的工具类添加测字符串省略

--- src/main/java/com/peter/common/string/StringUtil.java
+++ src/main/java/com/peter/common/string/StringUtil.java
@@ -1,13 +1,17 @@
 package com.peter.common.string;
 
+
+import org.springframework.util.StringUtils;
+
 /**
- * @ClassName StringUtils
+ * @ClassName StringUtil
  * @Description 关于字符串相关的方法
  * @Author peter
  * @Date 2019/7/29 13:54
  * @Version 1.0
  */
-public class StringUtils {
+public class StringUtil {
+
     /**
      * 转换成字符串方法，其中如果是Integer格式的返回0，如果是Double格式的返回0.0
      *
@@ -66,11 +70,39 @@ public class StringUtils {
 
 
     /**
-     * ---------------测试---------------
+     * 用户身份证号码的打码隐藏加星号加* 18位和非18位身份证处理均可成功处理 参数异常直接返回null
      *
-     * @param args
+     * @param idCardNum 身份证号码或者银行卡号
+     * @param front     需要显示前几位
+     * @param end       需要显示末几位
+     * @return 处理完成的身字符串
      */
-    public static void main(String[] args) {
+    public static String cardNumMask(String idCardNum, int front, int end) {
+        //字符串不能为空
+        if (StringUtils.isEmpty(idCardNum)) {
+            return null;
+        }
+        //需要截取的长度不能大于身份证号长度
+        if ((front + end) > idCardNum.length()) {
+            return null;
+        }
+        //需要截取的不能小于0
+        if (front < 0 || end < 0) {
+            return null;
+        }
+        //计算*的数量
+        int asteriskCount = idCardNum.length() - (front + end);
+        StringBuffer asteriskStr = new StringBuffer();
+        for (int i = 0; i < asteriskCount; i++) {
+            asteriskStr.append("*");
+        }
+        String regex = "(\\w{" + String.valueOf(front) + "})(\\w+)(\\w{" + String.valueOf(end) + "})";
+        return idCardNum.replaceAll(regex, "$1" + asteriskStr + "$3");
+    }
 
+
+
+    public static void main(String[] args) {
+        System.out.println(cardNumMask("6227003241901588939", 6, 4));
     }
 }
