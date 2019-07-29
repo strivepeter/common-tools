b65c66dee79b5ddc889235415fcec8fb276008de
zhangheng <zhangheng@moguyun.com>
2019-07-29 15:41:03 +0800

    初始化工程

---
+++ src/main/java/com/peter/CommonToolsApplication.java
@@ -0,0 +1,13 @@
+package com.peter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonToolsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/peter/common/test.java
@@ -0,0 +1,13 @@
+package com.peter.common;/**
+ * Created by DELL on 2019/7/29.
+ */
+
+/**
+ * @ClassName test
+ * @Description TODO
+ * @Author DELL
+ * @Date 2019/7/29 15:33
+ * @Version 1.0
+ */
+public class test {
+}
